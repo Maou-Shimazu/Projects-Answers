ab61d4ecd604abdd67b60671258dbef93901944a
Trevonir <102905107+Trevonir@users.noreply.github.com>
2022-04-18 18:52:38 +0200

    Written without an IDE; don't care if it works.

---
+++ Text/Palindrome/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Enter a word: ");
+        final String s = new Scanner(System.in).nextLine();
+        if (s.equals(new StringBuilder(s).reverse().toString())
+            System.out.println("The word is a palindrome.");
+        else
+            System.out.println("The word is not a palindrome.");
+    }
+}
