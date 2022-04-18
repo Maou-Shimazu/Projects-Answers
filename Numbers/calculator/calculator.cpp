8e5eff28fdc10f29c17646a635ae947ecfd6bb30
Maou-Shimazu <eldaddanladi@gmail.com>
2022-04-17 18:23:07 -0600

    calculator cpp

--- Numbers/calculator/calculator.cpp
+++ Numbers/calculator/calculator.cpp
@@ -1,108 +1,211 @@
 // Calculator repl
-#include <string>
+#include <assert.h>
+
 #include <iostream>
 #include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
 #include <math.h>
-using std::stringstream;
 
-int precedence(char op){
-    if(op == '+'||op == '-')
-        return 1;
-    if(op == '*'||op == '/')
-        return 2;
-    if(op == '^')
-        return 3;
-    return 0;
+using namespace std;
+
+enum TokKind {
+  NUM = 256,
+  ERROR,
+  EOE,
+};
+
+struct Tok {
+  int kind;
+  string text;
+};
+
+// Current token
+Tok ctok;
+
+struct Expr {
+  string& s;
+  int ptr = 0;
+};
+
+unordered_map<char, int> BinOpPrec = {
+    {'^', 3}, {'*', 2}, {'/', 2}, {'+', 1}, {'-', 1},
+};
+
+void lex(Expr& E) {
+  if (E.ptr >= E.s.length()) {
+    ctok = Tok{EOE};
+    return;
+  }
+  while (isspace(E.s[E.ptr])) {
+    ++E.ptr;
+  }  // skip whitespace
+
+  char c = E.s[E.ptr++];
+  switch (c) {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+    case '^':
+    case '(':
+    case ')': {
+      ctok = Tok{c, string(1, c)};
+      return;
+    }
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9': {
+      int spos = E.ptr - 1;
+      while (E.ptr < E.s.length() && isdigit(E.s[E.ptr])) {
+        ++E.ptr;
+      }
+      ctok = Tok{NUM, E.s.substr(spos, E.s.length())};
+      return;
+    }
+    default: {
+      if (c >= 33 && c <= 126) {
+        cout << "Error: unexpected char: " << c << '\n';
+      } else {
+        cout << "Error: unexpected char: " << (int)c << '\n';
+      }
+      ctok = Tok{TokKind::ERROR};
+      return;
+    }
+  }
 }
 
-int applyOp(int a, int b, char op){
-    switch(op){
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-        case '^': return pow(a, b);
+tuple<float, bool> expr(Expr& E);
+
+tuple<float, bool> fact(Expr& E) {
+  lex(E);
+  switch (ctok.kind) {
+    case NUM:
+      return {stof(ctok.text), true};
+    case '(': {
+      auto [R, ok] = expr(E);
+      if (!ok) return {0, false};
+      if (ctok.kind != ')') {
+        cout << "Error: expected closing ')'\n";
+        return {0, false};
+      }
+      return {R, true};
     }
-    return 0;
+    default:
+      cout << "Error: expected valid expression\n";
+      return {0, false};
+  }
 }
 
-int eval(std::string tokens){
-    int i;
-    std::stack <int> values;
-    std::stack <char> ops;
-     
-    for(i = 0; i < tokens.length(); i++){
-        if(tokens[i] == ' ')
-            continue;
-        else if(tokens[i] == '('){
-            ops.push(tokens[i]);
-        }
-        else if(isdigit(tokens[i])){
-            int val = 0;
-            while(i < tokens.length() && isdigit(tokens[i])){
-                val = (val*10) + (tokens[i]-'0');
-                i++;
-            }
-            values.push(val);
-              i--;
-        }
-        else if(tokens[i] == ')'){
-            while(!ops.empty() && ops.top() != '('){
-                int val2 = values.top();
-                values.pop();
-                 
-                int val1 = values.top();
-                values.pop();
-                 
-                char op = ops.top();
-                ops.pop();
-                 
-                values.push(applyOp(val1, val2, op));
-            }
-             
-            if(!ops.empty())
-               ops.pop();
-        }
-         
-        else{
-            while(!ops.empty() && precedence(ops.top())
-                                >= precedence(tokens[i])){
-                int val2 = values.top();
-                values.pop();
-                int val1 = values.top();
-                values.pop();
-                char op = ops.top();
-                ops.pop();
-                values.push(applyOp(val1, val2, op));
-            }
-            ops.push(tokens[i]);
-        }
-    }
-    while(!ops.empty()){
-        int val2 = values.top();
-        values.pop();
-        int val1 = values.top();
-        values.pop();
-        char op = ops.top();
-        ops.pop();
-        values.push(applyOp(val1, val2, op));
-    }
-     
-    return values.top();
+tuple<float, bool> apply(float LHS, float RHS, int op) {
+  switch (op) {
+    case '+':
+      return {LHS + RHS, true};
+    case '-':
+      return {LHS - RHS, true};
+    case '/':
+      // Might be more correct to do epsilon check
+      // here :)
+      if (RHS == 0) {
+        cout << "Tried to divide by zero\n";
+        return {0, false};
+      }
+      return {LHS / RHS, true};
+    case '*':
+      return {LHS * RHS, true};
+    case '^':
+      return {pow(LHS, RHS), true};
+    default:
+      assert("unreachable");
+      break;
+  }
+  return {0, false};
 }
 
-int main(){
-    std::cout << R"(
+tuple<float, bool> expr(Expr& E) {
+  auto [res, ok] = fact(E);
+  if (!ok) return {0, false};
+  float LHS = res;
+
+  struct Back {
+    Tok Op;
+    float R;
+  };
+
+  lex(E);
+  Tok Op = ctok;
+  if (Op.kind == ERROR) return {0, false};
+
+  stack<Back> OpStack;
+  while (BinOpPrec.find(Op.kind) != BinOpPrec.end()) {
+    auto [res, ok] = fact(E);
+    if (!ok) return {0, false};
+    float RHS = res;
+
+    lex(E);
+    Tok NextOp = ctok;
+    if (NextOp.kind == ERROR) return {0, false};
+    bool More = BinOpPrec.find(NextOp.kind) != BinOpPrec.end();
+    if (More && BinOpPrec[NextOp.kind] > BinOpPrec[Op.kind]) {
+      Back B = {Op, LHS};
+      LHS = RHS;
+      Op = NextOp;
+      OpStack.push(B);
+    } else {
+      auto [L, ok] = apply(LHS, RHS, Op.kind);
+      if (!ok) return {0, false};
+      LHS = L;
+
+      while (!OpStack.empty()) {
+        RHS = LHS;
+        Back B = OpStack.top();
+        if (More && BinOpPrec[NextOp.kind] > BinOpPrec[B.Op.kind]) {
+          LHS = RHS;
+          Op = NextOp;
+          break;
+        }
+
+        OpStack.pop();
+        LHS = B.R;
+
+        auto [L, ok] = apply(LHS, RHS, B.Op.kind);
+        if (!ok) return {0, false};
+        LHS = L;
+      }
+      Op = NextOp;
+    }
+  }
+  return {LHS, true};
+}
+
+int main() {
+  std::cout << R"(
 Basic C++ Calculator
 ________________________
 Enter an expression to evaluate.
-Note: +, -, *, /, ^ are supported, and no spaces are allowed. ctrl+c to exit.
+Note: Operators +, -, *, / and ^ available. ctrl+c to exit.
 )" << std::endl;
-    while(true){
-        std::string input;
-        std::cout << ">>> ";
-        std::cin >> input;
-        std::cout << eval(input) << std::endl;
+  while (true) {
+    cout << ">>> ";
+    string s;
+    std::getline(cin, s);
+    if (s.empty()) {
+      cout << "No input\n";
+      continue;
     }
-    
+
+    Expr E = {s};
+    auto [res, ok] = expr(E);
+    if (!ok) continue;
+
+    cout << res << '\n';
+  }
 }
