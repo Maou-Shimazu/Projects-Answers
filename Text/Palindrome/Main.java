93f8dfcfcdd35845fef63d33e33a90d9a7fad93f
Trevonir <102905107+Trevonir@users.noreply.github.com>
2022-04-18 18:53:34 +0200

    Update Main.java

--- Text/Palindrome/Main.java
+++ Text/Palindrome/Main.java
@@ -4,7 +4,7 @@ class Palindrome {
     public static void main(String[] args) {
         System.out.println("Enter a word: ");
         final String s = new Scanner(System.in).nextLine();
-        if (s.equals(new StringBuilder(s).reverse().toString())
+        if (s.equals(new StringBuilder(s).reverse().toString()))
             System.out.println("The word is a palindrome.");
         else
             System.out.println("The word is not a palindrome.");
