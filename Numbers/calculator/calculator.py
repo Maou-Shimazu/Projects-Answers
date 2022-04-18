07658b8c220baf86a6020533ad91c429f8838abe
1305 <1305@national.shitposting.agency>
2022-04-18 02:03:16 -0500

    make python calculator good
    ez

--- Numbers/calculator/calculator.py
+++ Numbers/calculator/calculator.py
@@ -1,25 +1,6 @@
-print("Easy Python Calculator")
-print('''
--- Options --
-> Addition / addition / +
-> Subtraction / subtraction / -
-> Multiplication / multiplication / *
-> Division / division / /
-''')
-operator = input("Enter the desired operator... ")
-fnum = float(input("Enter the first number... "))
-snum = float(input("Enter the second number... "))
-if operator == "Addition" or "addition" or "+":
-    result = fnum + snum
-    print(f"Your result: {result}")
-elif operator == "Subtraction" or "subtraction" or "-":
-    result = fnum - snum
-    print(f"Your result: {result}")
-elif operator == "Multiplication" or "multiplication" or "*":
-    result = fnum * snum
-    print(f"Your result: {result}")
-elif operator == "Division" or "division" or "/":
-    result = fnum / snum
-    print(f"Your result: {result}")
-else:
-    print("Invalid input")
+while True:
+  eq = input("pythonCalcRepl: ")
+  if eq.upper().isupper() == True:
+    print("Invalid equation.")
+  else:
+    print(eval(eq))
