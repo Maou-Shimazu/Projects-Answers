968d055f5d3e6c4c56bd5f2fe8061bcbd7324c6d
1305 <1305@national.shitposting.agency>
2022-04-18 03:05:58 +0000

    i made it not suck
    :3

--- Text/reverse-string/Program.cs
+++ Text/reverse-string/Program.cs
@@ -1,81 +1,30 @@
 using System;
 
-namespace reverse_str_hehe
+namespace actual_str_reverse
 {
-    class e
+    class Reverser
     {
-        public static string xz12;
-        public static char[] x812l;
-        public static char[] z86;
-        public static void z12(string x)
+        public static string reverse(string toReverse)
         {
-            xz12 = x;
-            x812l = xz12.ToCharArray();
-            Array.Reverse(x812l);
-            z86 = x812l;
-            xz12 = new String(z86);
-            xz12 = xz12;
+            string reversed = "";
+            int originalLen = toReverse.Length -1;
+            while (originalLen >= 0)
+            {
+                reversed = reversed + toReverse[originalLen];
+                originalLen--;
+            }
+            return reversed;
         }
     }
-
     class Program
     {
-        public static bool three;
-        public static string x827;
-        public static e a12z33333;
-        public static string z2z7x8;
-        static int TRUE(string e)
-        {
-            return 0;
-        }
-        static bool False(string eight)
-        {
-            if (eight == "88.62twelve")
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        static int eightyeightpointsixtytwotwelve(string x1b)
-        {
-            if (x1b == "88.62twelve")
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        static string Getline(int x8127)
-        {
-            if (x8127 == 1)
-            {
-                System.Environment.Exit(1);
-            }
-            else
-            {
-                Console.Write("Text to reverse: ");
-                x827 = Console.ReadLine();
-                return new string(x827.Replace(" ", " "));
-            }
-            return "";
-        }
         static void Main(string[] args)
         {
-            while (TRUE("*@#*(#@%&#@%&(*") != 1)
+            while (true)
             {
-                three = False("!@*($&!@$&*(&*(@$!&*(HI");
-                if (three == true)
-                {
-                    z2z7x8 = Getline(eightyeightpointsixtytwotwelve("88.62twelve\n"));
-                    a12z33333 = new e();
-                    e.z12(z2z7x8);
-                    Console.WriteLine(e.xz12);
-                }
+                Console.Write("To reverse: ");
+                string reverse = Console.ReadLine();
+                Console.WriteLine(Reverser.reverse(reverse));
             }
         }
     }
