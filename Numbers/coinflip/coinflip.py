3a5e153c7ddf0daa8f8bde6c393b2c4e16d5cc1e
Brandon <93276748+BrandonDoesDev@users.noreply.github.com>
2022-04-18 21:20:13 -0400

    Update coinflip.py
    Removed redundant list usage

--- Numbers/coinflip/coinflip.py
+++ Numbers/coinflip/coinflip.py
@@ -3,10 +3,7 @@ import random
 count = input("Enter the amount of times you want the coin flipped... ")
 
 choices = ["Heads", "Tails"]
-results = []
 
 for i in range(0, int(count)):
     result = random.choice(choices)
-    results.append(result)
-
-print(results)
+    print(results)
