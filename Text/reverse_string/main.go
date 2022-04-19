a1da94c89082b674b97d123a755fb7efb99558ca
Brandon <93276748+BrandonDoesDev@users.noreply.github.com>
2022-04-18 21:11:36 -0400

    Update main.go

--- Text/reverse_string/main.go
+++ Text/reverse_string/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Enter the string you want reversed...")
+    fmt.Println("Enter the string you want reversed...")
     var str string
     fmt.Scan(&str)
     for i := len(str); i > 0; i-- {
