7b6a2c6437db88d0d6ed7269d626ca9510e0e64b
Brandon <93276748+BrandonDoesDev@users.noreply.github.com>
2022-04-20 11:37:01 -0400

    Update binary_to_decimal.go

--- Numbers/binary_to_decimal/binary_to_decimal.go
+++ Numbers/binary_to_decimal/binary_to_decimal.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
   "strconv"
 )
 
 func main() {
-	fmt.Println("Enter your binary number...")
+  fmt.Println("Enter your binary number...")
   var input string
   fmt.Scan(&input)
   decimal, _ := strconv.ParseInt(input, 2, 64)  
