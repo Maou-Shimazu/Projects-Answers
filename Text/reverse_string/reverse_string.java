000ee3c2cb5c67341a33198119495eec5b31cb82
Elite <mrcalebf@gmail.com>
2022-04-21 02:22:01 -0400

    Update and rename ReverseString.java to reverse_string.java
    update naming for consistency

--- Text/reverse_string/reverse_string.java
+++ Text/reverse_string/reverse_string.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseString {
+public class reverse_string {
     public static void main(String[] args) {
         System.out.println("Enter the string you want reversed...");
         System.out.println(new StringBuilder(new Scanner(System.in).nextLine()).reverse());
