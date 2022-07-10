cc1cb67902f12b652271d05b82b0e47b355f2440
iaacornus <iaacornus.devel@gmail.com>
2022-07-10 22:53:31 +0800

    optimizations

--- Classic_Algorithms/sieve_of_eratosthenese/sieve_of_erastosthenes2.py
+++ Classic_Algorithms/sieve_of_eratosthenese/sieve_of_erastosthenes2.py
@@ -3,14 +3,9 @@ def erastosthenes(num: int) -> set[int]:
 
     evens: set[int] = set()
     for x in range(2, num):
-        if x == 2:
-            for y in range(2, num, 2):
-                if y > x:
-                    evens.add(y)
-        else:
-            for y in range(x, num, x):
-                if y > x:
-                    evens.add(y)
+        for y in range(x, num, x):
+            if y > x:
+                evens.add(y)
 
     odds: set[int] = {x for x in range(1, num)} - evens
     return sorted(odds)
